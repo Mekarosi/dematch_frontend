9bf6013f3a8a1db38cd3220dccf0dd8db04fddb7
Mekarosi <mekarosi_assa@yahoo.com>
2022-12-02 06:59:28 -0800

    Added some dependencies packages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 
@@ -6,10 +7,12 @@ import './App.css';
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={ Landing } />
+      </Fragment>
+    </Router>
   );
 }
 
