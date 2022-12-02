ec03b4928c510ce7d68c022e90f4acaf34422265
Mekarosi <mekarosi_assa@yahoo.com>
2022-12-02 07:24:52 -0800

    Added auth components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 import './App.css';
 
@@ -11,6 +13,12 @@ const App = () => {
       <Fragment>
         <Navbar />
         <Route exact path="/" component={ Landing } />
+        <section className='container'>
+          <Switch>
+            <Route exact path="/register" component={ Register } />
+            <Route exact path="/login" component={ Login } />
+          </Switch>
+        </section>
       </Fragment>
     </Router>
   );
