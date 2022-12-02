05ab7b1f703d9319000e4cc39fb5e82b8ab1073c
Mekarosi <mekarosi_assa@yahoo.com>
2022-12-02 06:24:37 -0800

    Added navbar and land components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react'
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
 import './App.css';
 
 const App = () => {
   return (
     <Fragment>
-      <h1>App</h1>
+      <Navbar />
+      <Landing />
     </Fragment>
   );
 }
