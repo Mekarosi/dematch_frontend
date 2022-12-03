9987b2dfde8f7049cc53d8cd8be9cfd9362c7873
Mekarosi <mekarosi_assa@yahoo.com>
2022-12-03 12:13:34 -0800

    Created action and reducer to login user

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+
 //Redux
 import { Provider } from 'react-redux'
 import store from './store'
+import setAuthToken  from './utils/setAuthToken'
+import { loadUser } from './actions/auth'
 
 
 import './App.css';
 
+if(localStorage.token) {
+  setAuthToken(localStorage.token)
+ } 
+
 const App = () => {
+
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
+
   return (
     <Provider store={store}>
     <Router>
--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,6 +1,8 @@
 import { 
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR
  } from '../actions/types'
 
  const initialState = {
@@ -13,8 +15,15 @@ import {
  function authReducer(state = initialState, action){
 
     const {type, payload} = action
-
+ 
     switch(type){
+        case USER_LOADED:
+            return {
+                ...state, 
+                isAuthenticated: true,
+                loading: false,
+                user: payload
+            }
         case REGISTER_SUCCESS:
            localStorage.setItem('token', payload.token) 
            return {
@@ -24,6 +33,7 @@ import {
              loading: false
            }
         case REGISTER_FAIL:  
+        case AUTH_ERROR:
            localStorage.removeItem('token') 
            return {
             ...state, 
