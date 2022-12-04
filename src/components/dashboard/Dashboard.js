a6d292363b7faa2df8ec4c98195f6c7363cf4511
Mekarosi <mekarosi_assa@yahoo.com>
2022-12-04 13:39:31 -0800

    Created profile action and reducer to get current user

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,7 +1,14 @@
-import React from 'react'
-// import PropTypes from 'prop-types'
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { getCurrentProfile } from '../../actions/profile'
 
-const Dashboard = props => {
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+
+    useEffect(() => {
+        getCurrentProfile()
+    }, [])
+
     return (
         <div>
             Dashboard
@@ -10,7 +17,14 @@ const Dashboard = props => {
 }
 
 Dashboard.propTypes = {
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired,
+ }
+ 
+ const mapStateToProps = state => ({
+     auth: state.auth,
+     profile: state.profile
+ })
 
-}
-
-export default Dashboard
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard)
