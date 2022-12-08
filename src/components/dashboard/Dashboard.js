42f6a2ecf03b49775d075ecdad44c8c5d665d451
Mekarosi <mekarosi_assa@yahoo.com>
2022-12-08 12:06:31 -0800

    Created components to add education and experience list

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
 import DashboardActions from './DashboardActions'
+import Experience from './Experience'
+import Education from './Education'
 import { getCurrentProfile } from '../../actions/profile'
 
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
@@ -22,6 +24,8 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
         {profile !== null ? 
         (<Fragment>
             <DashboardActions />
+            <Experience experience={profile.experience} />
+            <Education education={profile.education} />
         </Fragment> ): 
         (<Fragment>
             <p>You have not yet setup a profile, please add some info</p>
