65c822ab7ac3c846a639fe31514f4881b8739ddb
Mekarosi <mekarosi_assa@yahoo.com>
2022-12-09 07:26:01 -0800

    Created and displayed users profile

--- src/components/profiles/Profiles.js
+++ src/components/profiles/Profiles.js
@@ -6,38 +6,49 @@ import ProfileItem from './ProfileItem'
 import { getProfiles } from '../../actions/profile'
 
 const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
-   
+
     useEffect(() => {
         getProfiles()
-    }, [])
-   
-    return <Fragment>
-        { loading ? <Spinner/> : <Fragment>
+    }, [getProfiles])
+
+
+    return (
+        
+        <Fragment>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <Fragment>
             <h1 className='large text-primary'>Developers</h1>
             <p className='lead'>
-                <i className='fab fa-connectdevelop'></i>
-                Browser and connect with developers
+              <i className='fab fa-connectdevelop' /> Browse and connect with
+              developers
             </p>
             <div className='profiles'>
-                {profiles.length > 0 ? (
-                   profiles.map(profile => (
-                    <ProfileItem key={profile._id} profile={profile}/>
-                   )) 
-                ) : <h4>No profile found...</h4>}
+              {profiles.length > 0 ? (
+                profiles.map(profile => (
+                  <ProfileItem key={profile._id} profile={profile}  />
+                ))
+              ) : (
+                <h4>No profiles found...</h4>
+              )}
             </div>
-        </Fragment> }
-    </Fragment>
+          </Fragment>
+        )}
+      </Fragment>
+    )
 }
 
-
 Profiles.propTypes = {
-   getProfiles: PropTypes.func.isRequired,
-   profile: PropTypes.object.isRequired
+    getProfiles: PropTypes.func.isRequired,
+    profile: PropTypes.object.isRequired,
+   
 }
 
 const mapStateToProps = state => ({
     profile: state.profile
+    
+
 })
 
-
-export default connect(mapStateToProps, { getProfiles })(Profiles) 
+export default connect(mapStateToProps, { getProfiles })(Profiles)
