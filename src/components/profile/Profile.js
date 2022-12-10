1a164d4f638571b72f0fda6cd05c622cb5495826
Mekarosi <mekarosi_assa@yahoo.com>
2022-12-10 09:13:38 -0800

    Created profile education component

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -5,6 +5,7 @@ import Spinner from '../layout/Spinner'
 import ProfileTop from './ProfileTop'
 import ProfileAbout from './ProfileAbout'
 import ProfileExperience from './ProfileExperience'
+import ProfileEducation from './ProfileEducation'
 import { getProfileById } from '../../actions/profile'
 import { Link, useParams } from 'react-router-dom'
 
@@ -34,6 +35,14 @@ const Profile = ({ getProfileById, profile: { profile,loading }, auth }) => {
                             <ProfileExperience key={experience._id} experience={experience} />
                         ))}
                     </Fragment>) : (<h4>No experience credentials</h4>)}
+                  </div>
+                  <div className='profile-edu bg-white pp-2'>
+                    <h2 className='text-primary'>Education</h2>
+                    {profile.education.length > 0 ? (<Fragment>
+                        {profile.education.map(education => (
+                            <ProfileEducation key={education._id} education={education} />
+                        ))}
+                    </Fragment>) : (<h4>No education credentials</h4>)}
                   </div>
               </div> 
             </Fragment>}
