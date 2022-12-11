e1ecde9074e528e5ab1ea69e82913da870cf9a08
Mekarosi <mekarosi_assa@yahoo.com>
2022-12-11 07:04:57 -0800

    Displayed comments

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -4,7 +4,8 @@ import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
 import PostItem from '../posts/PostItem'
 import { getPost } from '../../actions/post'
-import CommentForm from './CommentForm'
+import CommentForm from '../post/CommentForm'
+import CommentItem from '../post/CommentItem'
 import { Link, useParams } from 'react-router-dom'
 
 const Post = ({ getPost, post:{ post, loading } }) => {
@@ -25,6 +26,11 @@ const Post = ({ getPost, post:{ post, loading } }) => {
         </Link>
         <PostItem  post={post}  showActions={false}/>
         <CommentForm postId={post._id} />
+        <div className='comments'>
+            {post.comments.map( comment => (
+                <CommentItem key={comment._id} comment={comment} postId={post._id} />
+            ) )}
+        </div>
     </Fragment>)
 }
 
